2b602e99f1b2df3bdfde52165171a0405a1eaee6
gastevend <steven.duval@libertymutual.com>
2017-05-12 17:00:16 -0400

    writes the scripts to test the current use changes.
    wrote the scripts for the update controller method to test to make sure
    someone else could not update another users songs. I also changed the route
    in the last commit to only allow index, update, create, and my personal
    non-restful routes. destroy and show arnt even used so they dont have routes.
    I might just rip those methods out in the next commit

--- scripts/user_songs/update-user_songs.sh
+++ scripts/user_songs/update-user_songs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 API="${API_ORIGIN:-http://localhost:4741}"
-URL_PATH="/user_songs/2"
+URL_PATH="/user_songs/78"
+TOKEN=BAhJIiViNzVkMzVlNWJjMTA2NzE1NGY1OGJiNTM3YzM3OTU4NQY6BkVG--661d30633aad11e6b8b5a03bb514023b0d59a87e
 curl "${API}${URL_PATH}" \
   --include \
   --request PATCH \
   --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN" \
   --data '{
     "user_song": {
       "song_id": "2",
